82fbebc05961272dbabd343f5854799833cdb842
Tommy Boi <tomaarongrabowski@gmail.com>
2024-05-11 13:30:47 -0400

    feat: Add interface for SqlDataAccess

---
+++ DataAccessLibrary/Data/ISqlDataAccess.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAccessLibrary;
+
+public interface ISqlDataAccess
+{
+    Task<IEnumerable<T>> LoadData<T, U>(string sql, U parameters, string connectionStringName);
+    Task SaveData<T>(string sql, T parameters, string connectionStringName);
+}
---
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace DataAccessLibrary;
+
+public class PersonModel
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
