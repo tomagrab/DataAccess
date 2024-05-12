eaa932a81cce11b0b1ea30c9b11031384b40336c
Tommy Boi <tomaarongrabowski@gmail.com>
2024-05-11 22:23:39 -0400

    chore: Update PersonModel to include validation attributes and update Home.razor and Read.razor components

--- DataAccessLibrary/Models/PersonModel.cs
+++ DataAccessLibrary/Models/PersonModel.cs
@@ -1,8 +1,14 @@
-﻿namespace DataAccessLibrary.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccessLibrary.Models;
 
 public class PersonModel
 {
     public int Id { get; set; }
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
+    [Required(ErrorMessage = "First name is required.")]
+    [MaxLength(50, ErrorMessage = "First name is too long.")]
+    public string? FirstName { get; set; }
+    [Required(ErrorMessage = "Last name is required.")]
+    [MaxLength(50, ErrorMessage = "Last name is too long.")]
+    public string? LastName { get; set; }
 }
